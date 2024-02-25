55b53d22a80fa8f653c6bdfb9d4a4c62a0de00d8
Drew Glavich <glavich.drew@optimind.tech>
2024-02-25 21:00:50 +0900

    chore: add strict mode for React components

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,24 +1,27 @@
 import { Stack } from "expo-router";
+import { StrictMode } from "react";
 import { PaperProvider } from "react-native-paper";
 
 import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <PaperProvider theme={theme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="mood-share"
-          options={{
-            headerTransparent: true,
-            headerTitle: "Mood Share",
-            headerTitleAlign: "center",
-            headerBackTitle: "Back",
-            headerBackTitleVisible: true,
-          }}
-        />
-      </Stack>
-    </PaperProvider>
+    <StrictMode>
+      <PaperProvider theme={theme}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="mood-share"
+            options={{
+              headerTransparent: true,
+              headerTitle: "Mood Share",
+              headerTitleAlign: "center",
+              headerBackTitle: "Back",
+              headerBackTitleVisible: true,
+            }}
+          />
+        </Stack>
+      </PaperProvider>
+    </StrictMode>
   );
 }
