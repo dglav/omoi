f77b478137784f6f70241e9370e68edbe9f674f0
Drew Glavich <glavich.drew@optimind.tech>
2024-02-25 21:00:17 +0900

    theme: override

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,8 @@ import { MD3LightTheme as DefaultTheme } from "react-native-paper";
 export const theme = {
   ...DefaultTheme,
   colors: {
+    ...DefaultTheme.colors,
     background: "#E8F3F7",
+    primary: "#18B66A",
   },
 };
