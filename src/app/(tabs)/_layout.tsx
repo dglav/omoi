5293b4d5eb09e217208e463327b2ecaf00e2e35f
Drew Glavich <glavich.drew@optimind.tech>
2024-02-26 13:15:43 +0900

    feat: add TabBar press color theme

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,14 +1,21 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import React from "react";
+import { useTheme } from "react-native-paper";
 
 const TabsLayout = () => {
+  const theme = useTheme();
+
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.primary,
+        headerShown: false,
+      }}
+    >
       <Tabs.Screen
         name="home"
         options={{
-          headerShown: false,
           title: "Home",
           tabBarIcon: ({ color }) => (
             <MaterialIcons name="home" size={24} color={color} />
@@ -18,7 +25,6 @@ const TabsLayout = () => {
       <Tabs.Screen
         name="questions-and-answers"
         options={{
-          headerShown: false,
           title: "Q&A",
           tabBarIcon: ({ color }) => (
             <MaterialIcons name="question-answer" size={24} color={color} />
@@ -28,7 +34,6 @@ const TabsLayout = () => {
       <Tabs.Screen
         name="mood-flow"
         options={{
-          headerShown: false,
           title: "Moodflow",
           tabBarIcon: ({ color }) => (
             <MaterialIcons name="monitor-heart" size={24} color={color} />
@@ -38,7 +43,6 @@ const TabsLayout = () => {
       <Tabs.Screen
         name="value-lists"
         options={{
-          headerShown: false,
           title: "Value lists",
           tabBarIcon: ({ color }) => (
             <MaterialIcons name="list-alt" size={24} color={color} />
