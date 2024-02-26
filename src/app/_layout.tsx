1d5df190ee6afb6fb9e89aaa21862650e5767bb9
Drew Glavich <glavich.drew@optimind.tech>
2024-02-26 12:33:38 +0900

    fix: add SafeAreaProvider as specified by the docs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack, useRouter } from "expo-router";
 import React, { StrictMode } from "react";
 import { Button, PaperProvider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { theme } from "../theme";
 
@@ -9,35 +10,37 @@ export default function Layout() {
 
   return (
     <StrictMode>
-      <PaperProvider theme={theme}>
-        <Stack>
-          <Stack.Screen
-            name="index"
-            options={{ headerTitle: "Login", headerShown: false }}
-          />
-          <Stack.Screen
-            name="register"
-            options={{
-              headerTitle: "Create Account",
-              headerTransparent: true,
-              headerRight: () => (
-                <Button onPress={() => router.push("/modal")}>Open</Button>
-              ),
-              headerBackTitle: "Back",
-            }}
-          />
-          <Stack.Screen
-            name="(tabs)"
-            options={{ headerShown: false, headerBackTitle: "Back" }}
-          />
-          <Stack.Screen
-            name="modal"
-            options={{
-              presentation: "modal",
-            }}
-          />
-        </Stack>
-      </PaperProvider>
+      <SafeAreaProvider>
+        <PaperProvider theme={theme}>
+          <Stack>
+            <Stack.Screen
+              name="index"
+              options={{ headerTitle: "Login", headerShown: false }}
+            />
+            <Stack.Screen
+              name="register"
+              options={{
+                headerTitle: "Create Account",
+                headerTransparent: true,
+                headerRight: () => (
+                  <Button onPress={() => router.push("/modal")}>Open</Button>
+                ),
+                headerBackTitle: "Back",
+              }}
+            />
+            <Stack.Screen
+              name="(tabs)"
+              options={{ headerShown: false, headerBackTitle: "Back" }}
+            />
+            <Stack.Screen
+              name="modal"
+              options={{
+                presentation: "modal",
+              }}
+            />
+          </Stack>
+        </PaperProvider>
+      </SafeAreaProvider>
     </StrictMode>
   );
 }
