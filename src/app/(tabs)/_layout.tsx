a70485bdda8df7fd05be752bfd93fff0d1ffff27
Drew Glavich <glavich.drew@optimind.tech>
2024-02-26 13:04:48 +0900

    feat: add TabBar icons

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import React from "react";
 
@@ -8,14 +9,42 @@ const TabsLayout = () => {
         name="home"
         options={{
           headerShown: false,
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="questions-and-answers"
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          title: "Q&A",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="question-answer" size={24} color={color} />
+          ),
+        }}
       />
-      <Tabs.Screen name="mood-flow" options={{ headerShown: false }} />
-      <Tabs.Screen name="value-lists" options={{ headerShown: false }} />
+      <Tabs.Screen
+        name="mood-flow"
+        options={{
+          headerShown: false,
+          title: "Moodflow",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="monitor-heart" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="value-lists"
+        options={{
+          headerShown: false,
+          title: "Value lists",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="list-alt" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
