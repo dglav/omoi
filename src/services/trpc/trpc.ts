ff3b44479548150b72a4d126f9b2ca73025fccd9
Drew Glavich <glavich.drew@optimind.tech>
2024-03-09 00:13:00 +0900

    feat: added trpc client

---
+++ src/services/trpc/trpc.ts
@@ -0,0 +1,13 @@
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+
+import type { AppRouter } from "../../../../backend/src";
+
+// Pass AppRouter as generic here. 👇 This lets the `trpc` object know
+// what procedures are available on the server and their input/output types.
+const trpc = createTRPCClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: "http://localhost:3001",
+    }),
+  ],
+});
