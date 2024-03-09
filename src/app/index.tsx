4a259d1aa21b89f53adc8492ddfb6d332999a300
Drew Glavich <glavich.drew@optimind.tech>
2024-03-09 23:07:49 +0900

    feat: added login screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,64 @@
-import { Link } from "expo-router";
-import { View, StyleSheet } from "react-native";
-import { useTheme } from "react-native-paper";
+import { useRouter } from "expo-router";
+import { View, StyleSheet, Text, Image } from "react-native";
+
+import { Button } from "../components/button";
+import { useAppTheme } from "../hooks/useAppTheme";
 
 export default function LoginPage() {
-  const theme = useTheme();
+  const theme = useAppTheme();
+  const router = useRouter();
 
   return (
     <View
-      style={{ ...styles.container, backgroundColor: theme.colors.background }}
+      style={{
+        ...styles.container,
+        backgroundColor: theme.colors.primary,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
     >
-      <Link href="/introduction">Intro</Link>
-      <Link href="/home">Login</Link>
-      <Link href="/register">Open Register</Link>
-      <Link href="/mood-share">Mood Share</Link>
+      <View style={{ flex: 1, marginTop: 160, gap: 80 }}>
+        <View>
+          <Text
+            style={{
+              color: theme.colors.white,
+              fontSize: theme.fontStyle.xxl[1].size,
+              fontWeight: theme.fontStyle.xxl[1].weight,
+              textAlign: "center",
+            }}
+          >
+            Omoi
+          </Text>
+          <Text
+            style={{
+              color: theme.colors.white,
+              fontSize: theme.fontStyle.xl[1].size,
+              fontWeight: theme.fontStyle.xl[1].weight,
+              textAlign: "center",
+            }}
+          >
+            β版
+          </Text>
+        </View>
+        <Image source={require("../../assets/Frame 184_white.png")} />
+      </View>
+      <View
+        style={{
+          width: "100%",
+          paddingVertical: 24,
+          paddingHorizontal: 16,
+          gap: 16,
+          height: 172,
+        }}
+      >
+        <Button variant="secondary" disabled>
+          新規アカウントを作る
+        </Button>
+        <Button variant="secondary" onPress={() => router.push("/home")}>
+          ログインする
+        </Button>
+      </View>
     </View>
   );
 }
