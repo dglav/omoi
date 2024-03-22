ccfeef7baac14f21e0efe0925dc3e6f782ff5d00
Drew Glavich <glavich.drew@optimind.tech>
2024-03-23 00:34:07 +0900

    feat: added a placeholder for me and parter toggles on home screen

--- src/app/(app)/(tabs)/home.tsx
+++ src/app/(app)/(tabs)/home.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Text } from "../../../components/text";
@@ -31,6 +31,43 @@ export default function HomePage() {
           Omoi
         </Text>
       </View>
+
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <Pressable
+          style={{
+            paddingVertical: 10,
+            paddingHorizontal: 16,
+            backgroundColor: "white",
+            borderRadius: 50,
+          }}
+        >
+          <Text>あなた</Text>
+        </Pressable>
+
+        <Pressable
+          style={{
+            paddingVertical: 10,
+            paddingHorizontal: 16,
+            borderRadius: 50,
+          }}
+        >
+          <Text>パートナー</Text>
+        </Pressable>
+      </View>
+
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: theme.colors.textLight,
+          marginTop: 8,
+        }}
+      />
 
       <View style={{ height: 40 }} />
 
