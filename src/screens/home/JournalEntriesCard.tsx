e3b435baf0466847aab2bb7cb37d87db1a4c37f2
Drew Glavich <glavich.drew@optimind.tech>
2024-05-06 17:33:46 +0900

    feat: add long press context menu

--- src/screens/home/JournalEntriesCard.tsx
+++ src/screens/home/JournalEntriesCard.tsx
@@ -3,6 +3,14 @@ import { View, Text } from "react-native";
 
 import { JournalEntryHeader } from "./JournalEntryHeader";
 import { JournalEntryRow } from "./JournalEntryRow";
+import {
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuItemIcon,
+  ContextMenuItemTitle,
+  ContextMenuRoot,
+  ContextMenuTrigger,
+} from "../../components/ContextMenu";
 import type { useGetPostGroups } from "../../hooks/postGroupHooks/useGetPostGroups";
 import { useAppTheme } from "../../hooks/useAppTheme";
 
@@ -63,42 +71,66 @@ export const JournalEntriesCard = ({ postGroup }: Props) => {
         </>
       )}
 
-      <View
-        style={{
-          backgroundColor: theme.colors.white,
-          width: "100%",
-          borderRadius: 16,
-          paddingVertical: 24,
-        }}
-      >
-        {postGroup.posts.map((post, index) => {
-          if (isToday && index === 0) {
-            return <JournalEntryHeader key={post.id} post={post} />;
-          }
-          return (
-            <View key={post.id}>
-              {isToday && index === 1 && (
-                <View
-                  style={{
-                    width: "100%",
-                    flexDirection: "row",
-                    justifyContent: "center",
-                  }}
-                >
-                  <View
-                    style={{
-                      height: 1,
-                      width: "92%",
-                      backgroundColor: theme.colors.textLight,
-                    }}
-                  />
+      <ContextMenuRoot style={{ borderRadius: 16 }}>
+        <ContextMenuTrigger>
+          <View
+            style={{
+              backgroundColor: theme.colors.white,
+              width: "100%",
+              borderRadius: 16,
+              paddingVertical: 24,
+            }}
+          >
+            {postGroup.posts.map((post, index) => {
+              if (isToday && index === 0) {
+                return <JournalEntryHeader key={post.id} post={post} />;
+              }
+              return (
+                <View key={post.id}>
+                  {isToday && index === 1 && (
+                    <View
+                      style={{
+                        width: "100%",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <View
+                        style={{
+                          height: 1,
+                          width: "92%",
+                          backgroundColor: theme.colors.textLight,
+                        }}
+                      />
+                    </View>
+                  )}
+                  <JournalEntryRow key={post.id} post={post} />
                 </View>
-              )}
-              <JournalEntryRow key={post.id} post={post} />
-            </View>
-          );
-        })}
-      </View>
+              );
+            })}
+          </View>
+        </ContextMenuTrigger>
+
+        <ContextMenuContent>
+          <ContextMenuItem key="edit">
+            <ContextMenuItemTitle>編集</ContextMenuItemTitle>
+            <ContextMenuItemIcon
+              ios={{ name: "pencil" }}
+              androidIconName="baseline_format_paint"
+            />
+          </ContextMenuItem>
+
+          <ContextMenuItem key="delete" destructive>
+            <ContextMenuItemTitle>最新の投稿を削除</ContextMenuItemTitle>
+            <ContextMenuItemIcon
+              ios={{ name: "trash" }}
+              androidIconName="baseline_delete"
+            />
+          </ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenuRoot>
+
+      {/* </ContextMenu> */}
     </View>
   );
 };
