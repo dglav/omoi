31853304a5f4ac708ac46033fcbc96cf58cc56fa
Drew Glavich <glavich.drew@optimind.tech>
2024-06-28 11:28:34 +0900

    chore: update app version

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default {
     owner: "dglav",
     name: "omoi",
     slug: "omoi",
-    version: "0.1.0",
+    version: "0.1.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "light",
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.dglav.omoi",
-      buildNumber: "0.1.0",
+      buildNumber: "0.1.1",
     },
     android: {
       adaptiveIcon: {
