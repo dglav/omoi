756a8fd8412cd011b38b5ff803425617f304c9c4
Drew Glavich <glavich.drew@optimind.tech>
2024-07-11 22:28:33 +0900

    feat: implemented tab swiping in log screen

--- src/screens/log/index.tsx
+++ src/screens/log/index.tsx
@@ -1,19 +1,61 @@
 import { useState } from "react";
+import PagerView from "react-native-pager-view";
 
 import { Container } from "../../components/FeedAndAnalysisHeader/Container";
 import { Header } from "../../components/FeedAndAnalysisHeader/Header";
 import MyLogScreen from "./MyLog";
 import PartnerLogScreen from "./PartnerLog";
+import { Animated, View } from "react-native";
+import { useNavigationPanel } from "../../hooks/useNavigationPanel";
+
+const AnimatedPagerView = Animated.createAnimatedComponent(PagerView);
 
 export default function LogScreen() {
   const [activeTab, setActiveTab] = useState<"me" | "partner">("me");
+  const { ref, ...navigationPanel } = useNavigationPanel((page) =>
+    setActiveTab(page === 0 ? "me" : "partner")
+  );
+
+  const handleSetActiveTab = (tab: "me" | "partner") => {
+    setActiveTab(tab);
+    navigationPanel.setPage(tab === "me" ? 0 : 1);
+  };
 
   return (
     <Container activeTab={activeTab}>
-      <Header title="Omoi" activeTab={activeTab} setActiveTab={setActiveTab} />
+      <Header
+        title="Omoi"
+        activeTab={activeTab}
+        setActiveTab={handleSetActiveTab}
+      />
 
-      {activeTab === "me" && <MyLogScreen />}
-      {activeTab === "partner" && <PartnerLogScreen />}
+      <AnimatedPagerView
+        ref={ref}
+        style={{ flex: 1 }}
+        initialPage={0}
+        layoutDirection="ltr"
+        overdrag={navigationPanel.overdragEnabled}
+        scrollEnabled={navigationPanel.scrollEnabled}
+        onPageScroll={navigationPanel.onPageScroll}
+        onPageSelected={navigationPanel.onPageSelected}
+        onPageScrollStateChanged={navigationPanel.onPageScrollStateChanged}
+        orientation="horizontal"
+      >
+        <View
+          key="1"
+          style={{ flex: 1 }}
+          collapsable={false}
+        >
+          <MyLogScreen />
+        </View>
+        <View
+          key="2"
+          style={{ flex: 1 }}
+          collapsable={false}
+        >
+          <PartnerLogScreen />
+        </View>
+      </AnimatedPagerView>
     </Container>
   );
 }
